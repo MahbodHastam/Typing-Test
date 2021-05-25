af741de6be0e196718c448085ac6980c62f827f8
Mahbod <mahbod4782@gmail.com>
2021-05-25 17:42:57 +0430

    little changes

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-(() => {
+// const getLastItem = array => array.slice(-1)[0]
+// const isLastItem = (array, item) => array[array.length - 1] === item ? item : null
+// const isFirstItem = (array, item) => array[0] === item ? item : null
+// const getNextItem = (array, index) => !isLastItem(array, array[index]) ? array[index + 1] : null
+
+;(() => {
   const words = [
     'hello', 'world', 'say', 'hi', 'bye', 'love', 'me', 'please', 'heh', 'huh', 'people', 'freeze', 'sleep',
   ]
@@ -16,6 +21,8 @@
   let currentWord = words[wordsCounter]
   const wordsElements = wordsElement.querySelectorAll('span')
 
+  wordsElements[wordsCounter].style.color = '#686868'
+
   input.onkeyup = function (e) {
     if (e.currentTarget.value.trim() === currentWord) {
       wordsElements[wordsCounter].style.color = 'royalblue'
@@ -23,7 +30,11 @@
       currentWord = words[wordsCounter]
       e.currentTarget.value = ''
     } else {
-      wordsElements[wordsCounter].style.color = '#e2413e'
+      if (e.currentTarget.value.trim().length <= currentWord.length) {
+        wordsElements[wordsCounter].style.color = '#686868'
+      } else {
+        wordsElements[wordsCounter].style.color = '#e2413e'
+      }
     }
   }
 
